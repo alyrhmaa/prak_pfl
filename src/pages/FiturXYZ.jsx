84fcedec4d3db35a7c1da54945625f9ce3a8aaff
alyrhmaa <aliya24si@mahasiswa.pcr.ac.id>
2026-06-04 15:37:45 +0700

    feat: tambah komponen navbar

--- src/pages/FiturXYZ.jsx
+++ src/pages/FiturXYZ.jsx
@@ -1,4 +1,15 @@
+import { Button } from "@/components/ui/button";
 import PageHeader from "../components/PageHeader";
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useState } from "react";
 import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
 
@@ -8,6 +19,35 @@ export default function FiturXYZ() {
       <PageHeader title="Dashboard" />
 
       <p>Ini halaman Fitur XYZ</p>
+
+      <Button>Simpan</Button>
+      <Button variant="outline">Simpan</Button>
+      <Button variant="secondary">Simpan</Button>
+      <Button variant="ghost" size="xs">
+        Simpan
+      </Button>
+      <div className="mt-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="mt-6">
+        <Badge>Default</Badge>
+        <Badge variant="secondary">Secondary</Badge>
+        <Badge variant="destructive">Destructive</Badge>
+        <Badge variant="outline">Outline</Badge>
+        <Badge variant="ghost">Ghost</Badge>
+      </div>
     </div>
   );
 }
